bc5f31daa20c20f74e62f29583967c212bd5a745
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2017-10-18 13:04:38 -0200

    Inicialização do Projeto

---
+++ eclipse/src/ele32_lab3/CanalBSC.java
@@ -0,0 +1,5 @@
+package ele32_lab3;
+
+public class CanalBSC {
+
+}
---
+++ eclipse/testes/ele32_lab3/TesteCanalBSC.java
@@ -0,0 +1,19 @@
+package ele32_lab3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TesteCanalBSC {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
