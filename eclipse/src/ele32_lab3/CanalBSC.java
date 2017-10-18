0a54687913dde676152bd59ce89b26ff9cf9ae4f
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2017-10-18 14:29:03 -0200

    Comentário TODO BSC

--- eclipse/src/ele32_lab3/CanalBSC.java
+++ eclipse/src/ele32_lab3/CanalBSC.java
@@ -2,4 +2,13 @@ package ele32_lab3;
 
 public class CanalBSC {
 
+	/**
+	 * Escreve um arquivo de saída embaralhado (trocas) com probabilidade definida
+	 * @param arquivoEntrada
+	 * @param arquivoSaida
+	 * @param probabiblidade
+	 */
+	public static void canaliza(String arquivoEntrada, String arquivoSaida, double probabiblidade) {
+		// TODO fazer
+	}
 }
